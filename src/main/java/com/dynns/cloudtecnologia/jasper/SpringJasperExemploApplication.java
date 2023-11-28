bc0fc00536139747446e2b76713f82e8a73a62b4
thi4go-git <thi4go19@gmail.com>
2023-11-28 20:32:17 -0300

    Commit inicial

---
+++ src/main/java/com/dynns/cloudtecnologia/jasper/SpringJasperExemploApplication.java
@@ -0,0 +1,13 @@
+package com.dynns.cloudtecnologia.jasper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJasperExemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJasperExemploApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dynns/cloudtecnologia/jasper/SpringJasperExemploApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dynns.cloudtecnologia.jasper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJasperExemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
